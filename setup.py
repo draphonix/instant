f6490729cccb51b232160d90b67ce0397ff7e8f1
HoangNB <hoangnb@gearinc.com>
2024-08-07 23:22:28 +0700

    Added env example

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click',
+        'click',
+        'python-dotenv',
     ],
     entry_points={
         'console_scripts': [
